822a0730a4bf090d50513b12dcf1a707cf76aec9
KaiGeller <gellerka@oregonstate.edu>
2022-04-06 21:57:17 -0700

    Project 2

---
+++ main.py
@@ -0,0 +1,16 @@
+# Author: Kai Geller
+# Github username: KaiGeller
+# Date: 4/6/2022
+print("Please Enter 5 Numbers")
+num_1 = int(input("Please enter a number here: "))
+num_2 = int(input("Please enter a number here: "))
+num_3 = int(input("Please enter a number here: "))
+num_4 = int(input("Please enter a number here: "))
+num_5 = int(input("Please enter a number here: "))
+print(num_1)
+print(num_2)
+print(num_3)
+print(num_4)
+print(num_5)
+sum = str((num_1 + num_2 + num_3 + num_4 + num_5)/5)
+print("the average of those numbers is " + sum + ".")
